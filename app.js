921434284fc1bb176018a497a032f92e0365ed8b
Fabian Häfliger <fabian.haefliger@students.fhnw.ch>
2021-11-16 22:31:27 +0100

    inner html and started with back project logic

--- app.js
+++ app.js
@@ -1,15 +1,48 @@
 import product from './product.js'
 
-let bookmarkIcon     =     document.querySelector('.bookmark')
-let hamburgerIcon     =     document.getElementById("hamburger-icon");
-let closeMenuIcon     =     document.getElementById("close-menu-icon")
-let menuIconDiv       =     document.getElementById("menu-icons")
-let navMenu           =     document.getElementById("nav")
-let overlay           =     document.getElementById("overlay")
-let backProjectButton =     document.getElementById("back-project-button")
-let backProjectModal  =     document.querySelector(".back-project-modal")
-let closeModalIcon    =     document.querySelector(".close-modal")
-let radioButtons      =     document.querySelectorAll(".pledge__radio");
+let bookmarkIcon        =     document.querySelector('.bookmark')
+let hamburgerIcon       =     document.getElementById("hamburger-icon");
+let closeMenuIcon       =     document.getElementById("close-menu-icon")
+let menuIconDiv         =     document.getElementById("menu-icons")
+let navMenu             =     document.getElementById("nav")
+let overlay             =     document.getElementById("overlay")
+let backProjectButton   =     document.getElementById("back-project-button")
+let backProjectModal    =     document.querySelector(".back-project-modal")
+let closeModalIcon      =     document.querySelector(".close-modal")
+
+
+let radioButtons        =     document.querySelectorAll(".pledge__radio");
+
+
+/* 
+      PRODUCT SELECTORS
+*/
+let fundingName         =     document.querySelector('.funding-name');   
+let fundingTotalFunded  =     document.querySelector('.funding_stat-total-funded');
+let fundingTotalBackers =     document.querySelector('.funding__stat-total-backers');
+let fundingDaysLeft     =     document.querySelector(".funding_stat-days-left");
+
+let pledgeTitles        =     document.querySelectorAll('.about__pledge-name');
+let pledgeNumberLeft    =     document.querySelectorAll('.about__pledge-left')
+
+let modalTitles         =     document.querySelectorAll(".modal-title");
+let modalNumbersLeft     =     document.querySelectorAll('.modal-number');
+
+for(let i = 0; i < pledgeTitles.length; i++){
+  pledgeTitles[i].innerHTML = product["pledges"][i]["name"];
+  pledgeNumberLeft[i].innerHTML = product["pledges"][i]["left"];
+  modalTitles[i].innerHTML = product["pledges"][i]["name"];
+  modalNumbersLeft[i].innerHTML = product["pledges"][i]["left"];
+}
+
+
+
+/* INIT FUNDING ELEMENTS */
+fundingName.innerHTML           = product["name"];
+fundingTotalFunded.innerHTML    = '$' + product["funded"];
+fundingTotalBackers.innerHTML   = product["noOfBacker"];
+fundingDaysLeft.innerHTML       = product["daysLeft"];
+
 
 menuIconDiv.addEventListener('click', () => {
   if(!hamburgerIcon.classList.contains('hidden')) {
@@ -48,9 +81,19 @@ closeModalIcon.addEventListener("click", () => {
 
 bookmarkIcon.addEventListener('click', () => {
   if(product["bookmarked"]) {
-    return;
+    product["bookmarked"] = false;
+    bookmarkIcon.src = "./images/icon-bookmark.svg";
   } else {
     product["bookmarked"] = true;
     bookmarkIcon.src = "./images/icon-check.svg";
   }
+})
+
+
+
+radioButtons.forEach((radioButton) => {
+  radioButton.addEventListener(('click'), () => {
+    let currentModal = radioButton.parentElement.parentElement;
+    currentModal.style.border = "1px solid var(--dark)"
+  })
 })
